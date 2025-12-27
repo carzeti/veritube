e1024c7de709667e405e753769da064889b93f5f
Carlos Zetina <53247037+carzeti@users.noreply.github.com>
2025-12-27 00:59:58 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
-// Importa las funciones que necesitamos de Firebase
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configuración de tu proyecto Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCv82J5kQ_9w1NlyL_VTUWQ8yzSWPmEkYo",
   authDomain: "veritube-a6d5c.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
   appId: "1:728082814598:web:98ca739c76aea1c4a92307"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta Auth y Firestore para usar en el proyecto
 export const auth = getAuth(app);
 export const db = getFirestore(app);
